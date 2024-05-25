fa873b67177de8709cc695488e831fce5a32a3f5
Sigma Enterprises <ceronode@tuta.io>
2024-05-25 14:50:08 -0400

    Relay and Favicon Update

--- ublog.config.js
+++ ublog.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: "parityday@vlt.ge Ncodes",
-  description:
-    "Every NIP-05 is a Unique expression of individuality.",
-favicon: "https://serve-model-sd-outputs.s3.amazonaws.com/e05a3789-23d0-4358-883b-f91553a7b76c.png", logo: "https://image.nostr.build/00b64f635fc23924690443924c3562698125faebfce7dad4f015e117c6508406.gif",
+  description:"Every NIP-05 is a Unique expression of individuality.",
+  logo:"https://image.nostr.build/ab420a445188a68e5fabe711cb0a6170cf7c35639abbe1f757ae01118557229a.gif"
+favicon: "https://image.nostr.build/thumb/31c4955dc0073f61b12b88e23b69cdb81046ab5b11df15f1acb8b4dd4e2d7070.gif", logo: "https://image.nostr.build/00b64f635fc23924690443924c3562698125faebfce7dad4f015e117c6508406.gif",
   npub: "npub16d8gxt2z4k9e8sdpc0yyqzf5gp0np09ls4lnn630qzxzvwpl0rgq5h4rzv",
   themes: [
     "light",
@@ -66,5 +66,6 @@ favicon: "https://serve-model-sd-outputs.s3.amazonaws.com/e05a3789-23d0-4358-883
      "wss://nostr-relay.wlvs.space",
      "wss://nostr.zebedee.cloud",
      "wss://relay.nostr.info",
+     "wss://libretechsystems.nostr1.com/"
   ],
 };
