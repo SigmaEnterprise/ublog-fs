97c6c96ba7c3ee6fb2ab55eb65c45aa691e1264e
Sigma Enterprises <ceronode@tuta.io>
2024-08-20 03:56:19 -0400

    Relay Update - All Restored

--- ublog.config.js
+++ ublog.config.js
@@ -54,5 +54,6 @@ favicon: "https://image.nostr.build/thumb/31c4955dc0073f61b12b88e23b69cdb81046ab
         "wss://nos.lol",
         "wss://atlas.nostr.land",
         "wss://relay.nostr.band",
+        "wss://libretechsystems.nostr1.com/",
   ],
 };
