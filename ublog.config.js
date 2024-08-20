c672daa2318b12ebe313695fca512484d178b60b
Sigma Enterprises <ceronode@tuta.io>
2024-08-20 03:29:05 -0400

    Relay Update - Nos.lol added

--- ublog.config.js
+++ ublog.config.js
@@ -50,10 +50,11 @@ favicon: "https://image.nostr.build/thumb/31c4955dc0073f61b12b88e23b69cdb81046ab
     },
   ],
   relays: [
+        "wss://nos.lol"
         "wss://relay.damus.io",
         "wss://nostr.nostrelay.org",
         "wss://atlas.nostr.land",
-        "wss://relay.nostr.info",
+        "wss://relay.nostr.band",
         "wss://libretechsystems.nostr1.com"
   ],
 };
